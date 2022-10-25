12fb913aac8abaa548020994b9c1c036ea49003e
97siwon <110653581+97siwon@users.noreply.github.com>
2022-10-25 16:12:37 +0900

    spring

--- project2/src/main/java/edu/kh/project/member/model/dao/MemberDAO.java
+++ project2/src/main/java/edu/kh/project/member/model/dao/MemberDAO.java
@@ -1,5 +1,7 @@
 package edu.kh.project.member.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import edu.kh.project.member.model.vo.Member;
@@ -8,14 +10,20 @@ import edu.kh.project.member.model.vo.Member;
                               // -> 서버를 꺼도 유지됨
 public class MemberDAO {
 
+	// DBCP + 마이바티스 이용 객체 DI(의존성 주입)
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	
 	/** 로그인 DAO
 	 * @param memberEmail
 	 * @return loginMember
 	 */
 	public Member login(String memberEmail) {
-
-
-		return null;
+		// 커넥션 대신 Mybatis 사용
+ 
+		//     sqlSession.selectOne("매퍼이름.태그id", SQL 작성 시 필요한 값);
+		return sqlSession.selectOne("memberMapper.login", memberEmail);
 	}
 
 }
