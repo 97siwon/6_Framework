21b1127ecf1b717547c78efb829f59f7e4fa29f1
97siwon <110653581+97siwon@users.noreply.github.com>
2022-11-18 16:34:28 +0900

    221118

--- project2/src/main/java/edu/kh/project/board/model/dao/BoardDAO.java
+++ project2/src/main/java/edu/kh/project/board/model/dao/BoardDAO.java
@@ -52,5 +52,14 @@ public class BoardDAO {
 		                             // namespace.id              , 파라미터 , RowBounds 객체
 		                             //                             파라미터가 없을 경우 null 대입
 	}
+
+	/** 게시물 상세 조회 + 이미지 목록 조회 + 댓글 목록 조회 DAO
+	 * @param boardNo
+	 * @return
+	 */
+	public Board selectBoardDetail(int boardNo) {
+		
+		return sqlSession.selectOne("boardMapper.selectBoardDetail", boardNo);
+	}
 	
 }
